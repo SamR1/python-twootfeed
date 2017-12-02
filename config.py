961d9a6106706bc62c02d848b59eba2086e3d1de
@gdorn@social.coop <georgedorn@gmail.com>
2017-12-01 21:39:08 -0800

    Set encoding for config file during read

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ Loads and parses the configuration file.
 import yaml
 
 def get_config():
-    with open('config.yml', 'r') as stream:
+    with open('config.yml', 'r', encoding='utf-8') as stream:
         try:
             return yaml.safe_load(stream)
         except yaml.YAMLError as e:

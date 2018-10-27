699ec1a85d786b6a57e5c6ab9e5e7e81b50ea432
Sam <sam@samr1.net>
2018-10-27 19:41:56 +0200

    refactor - twitter route

--- twootfeed/twitter/routes.py
+++ twootfeed/twitter/routes.py
@@ -10,89 +10,83 @@ from twootfeed.utils.feed_generation import generate_feed
 twitter_bp = Blueprint('twitter', __name__)
 
 
-def format_tweet(i):
-    tweet = {'text': i.full_text, 'screen_name': i.user.screen_name,
-             'profile_image_url': i.user.profile_image_url_https,
-             'user_name': i.user.name, 'user_url': i.user.url,
-             'id_str': i.id_str, 'created_at': i.created_at,
-             'source': i.source, 'retweets': i.retweet_count,
-             'favorites': i.favorite_count}
-    tweet['tweet_url'] = 'https://twitter.com/' + tweet[
-        'screen_name'] + '/status/' + tweet['id_str']
+def format_tweet(tweet):
+    rss_tweet = {'text': tweet.full_text, 'user_name': tweet.user.name,
+                 'screen_name': tweet.user.screen_name,
+                 'created_at': tweet.created_at}
 
-    tweet['htmltext'] = '<blockquote><div><img src="' + tweet[
-        'profile_image_url'] + \
-        '" alt="' + tweet['screen_name'] + \
-        '" />   <strong>' + tweet['user_name'] + \
-        ': </strong>' + tweet['text'] + \
-        '<br><i>Source: ' + tweet[
-            'source'] + '</i>'
+    rss_tweet['tweet_url'] = 'https://twitter.com/{}/status/{}'.format(
+        tweet.user.screen_name, tweet.id_str
+    )
 
-    user_mentionslist = i.entities.get('user_mentions')
+    rss_tweet['htmltext'] = ('<blockquote><div><img src="{}" alt="{}'.format(
+        tweet.user.profile_image_url_https, tweet.user.screen_name
+    ) + 'profile image"/> <strong>{}: </strong>{}<br><i>Source: {}'.format(
+        tweet.user.name, tweet.full_text, tweet.source
+    ) + '</i>')
+
+    user_mentionslist = tweet.entities.get('user_mentions')
     for user in user_mentionslist:
         if user != '':
-            tweet['htmltext'] = re.sub(
-                ('@' + user.get('screen_name')),
-                ('<a href="https://twitter.com/'
-                 + user.get('screen_name')
-                 + '" target="_blank">@'
-                 + user.get('screen_name') + '</a>'),
-                tweet['htmltext'])
+            screen_name = user.get('screen_name')
+            rss_tweet['htmltext'] = re.sub(
+                '@' + user.get('screen_name'),
+                ('<a href="https://twitter.com/{}" target="_blank">@{}'.format(
+                    screen_name, screen_name
+                ) + '</a>'),
+                rss_tweet['htmltext'])
 
-    hashtaglist = i.entities.get('hashtags')
+    hashtaglist = tweet.entities.get('hashtags')
     for hashtag in hashtaglist:
         if hashtag != '':
-            tweet['htmltext'] = re.sub(
-                ('#' + hashtag.get('text')),
-                ('<a href="https://twitter.com/hashtag/'
-                 + hashtag.get('text')
-                 + '?src=hash" target="_blank">#'
-                 + hashtag.get('text') + '</a>'),
-                tweet['htmltext']
+            tag = hashtag.get('text')
+            rss_tweet['htmltext'] = re.sub(
+                '#' + hashtag.get('text'),
+                ('<a href="https://twitter.com/hashtag/{}'.format(tag) +
+                 '?src=hash" target="_blank">#{}</a>'.format(tag)),
+                rss_tweet['htmltext']
             )
 
-    urllist = i.entities.get('urls')
+    urllist = tweet.entities.get('urls')
     for url in urllist:
         if url != '':
-            tweet['htmltext'] = re.sub(
-                (url.get('url')),
-                ('<a href="'
-                 + url.get('expanded_url')
-                 + '" target="_blank">'
-                 + url.get('display_url')
-                 + '</a>'),
-                tweet['htmltext'])
+            rss_tweet['htmltext'] = re.sub(
+                url.get('url'),
+                '<a href="{}" target="_blank">{}</a>'.format(
+                    url.get('expanded_url'), url.get('display_url')
+                ),
+                rss_tweet['htmltext'])
     try:
-        medialist = i.extended_entities.get('media')
+        medialist = tweet.extended_entities.get('media')
     except AttributeError:
         pass
     else:
         if medialist is not None:
-            tweet['htmltext'] = tweet['htmltext'] + '<br> '
+            rss_tweet['htmltext'] = rss_tweet['htmltext'] + '<br> '
             for media in medialist:
                 if media != '':
                     if media.get('type') == 'photo':
-                        tweet['htmltext'] = re.sub(
-                            media.get('url'), '',
-                            tweet['htmltext']
+                        media_url = media.get('media_url_https')
+                        rss_tweet['htmltext'] = re.sub(
+                            media.get('url'), '', rss_tweet['htmltext']
                         )
-                        tweet['htmltext'] = (tweet['htmltext'] + ' <a href="' +
-                                             media.get('media_url_https') +
-                                             '" target="_blank"><img src="' +
-                                             media.get('media_url_https') +
-                                             ':thumb' + '"></a>')
+                        rss_tweet['htmltext'] += ('<a href="{}" '.format(
+                            media_url
+                        ) + 'target="_blank"><img src="{}'.format(
+                            media_url
+                        ) + ':thumb"></a>')
 
-    location = i.place
+    location = tweet.place
     if location is not None:
-        tweet['htmltext'] = tweet['htmltext'] \
-                            + '<br><i>Location: ' \
-                            + location.full_name + '</i>'
+        rss_tweet['htmltext'] += '<br><i>Location: {}</i>'.format(
+            location.full_name
+        )
 
-    tweet['htmltext'] = tweet['htmltext'] + '<br> ♻ : ' + str(
-        tweet['retweets']) + ', ' + '♥ : ' + str(
-        tweet['favorites']) + '</div></blockquote>'
+    rss_tweet['htmltext'] += '<br> ♻ : {}, ♥ : {}</div></blockquote>'.format(
+        tweet.retweet_count, tweet.favorite_count
+    )
 
-    return tweet
+    return rss_tweet
 
 
 @twitter_bp.route('/<query_feed>', methods=['GET'])
@@ -105,32 +99,32 @@ def tweetfeed(query_feed):
         feed_link = param['twitter']['link'] + query_feed
         f = generate_feed(feed_title, feed_link, param)
 
-        for i in tweepy.Cursor(twitter_api.search,
-                               q=query_feed,
-                               tweet_mode='extended').items():
+        for tweet in tweepy.Cursor(twitter_api.search,
+                                   q=query_feed,
+                                   tweet_mode='extended').items():
             try:
-                i.full_text
+                tweet.full_text
             except Exception:
                 break
             else:
                 try:
-                    retweeted_status = i.retweeted_status
+                    retweeted_status = tweet.retweeted_status
 
                 except Exception:
                     retweeted_status = False
 
                 if not retweeted_status:  # only the original tweets
-                    tweet = format_tweet(i)
+                    formatted_tweet = format_tweet(tweet)
                     f.add_item(
-                        title=tweet['screen_name']
+                        title=formatted_tweet['screen_name']
                         + ' ('
-                        + tweet['user_name'] + '): '
-                        + tweet['text'],
-                        link=tweet['tweet_url'],
+                        + formatted_tweet['user_name'] + '): '
+                        + formatted_tweet['text'],
+                        link=formatted_tweet['tweet_url'],
                         pubdate=pytz.utc.localize(
-                            tweet['created_at']).astimezone(
+                            formatted_tweet['created_at']).astimezone(
                             pytz.timezone(param['feed']['timezone'])),
-                        description=tweet['htmltext'])
+                        description=formatted_tweet['htmltext'])
 
         xml = f.writeString('UTF-8')
     else:
